d8517efcc53acf5c34a19009786018389ea3e2f6
andrey <no_questions.dota2@mail.ru>
2024-08-14 02:56:15 +0300

    half website

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const reload = document.querySelector(".header__left");
 const telephoneBtn = document.querySelector(".right__telephone");
 
+const seaFreight = document.querySelector(".services__sea-freight");
+const railwayFreight = document.querySelector(".services__railway-freight");
+const airFreight = document.querySelector(".services__air-freight");
+const roadFreight = document.querySelector(".services__road-freight");
+const warehousingServices = document.querySelector(
+  ".services__warehousing-services"
+);
+const oversizedCargoesTransportation = document.querySelector(
+  ".services__oversized-cargoes-transportation"
+);
+
 reload.addEventListener("click", () => {
   location.reload();
   location.href = "index.html";
@@ -8,4 +19,28 @@ reload.addEventListener("click", () => {
 
 telephoneBtn.addEventListener("click", () => {
   window.open("tel:+74955325212", "_blank");
+});
+
+seaFreight.addEventListener("click", () => {
+  location.href = "#";
+});
+
+railwayFreight.addEventListener("click", () => {
+  location.href = "#";
+});
+
+airFreight.addEventListener("click", () => {
+  location.href = "#";
+});
+
+roadFreight.addEventListener("click", () => {
+  location.href = "#";
+});
+
+warehousingServices.addEventListener("click", () => {
+  location.href = "#";
+});
+
+oversizedCargoesTransportation.addEventListener("click", () => {
+  location.href = "#";
 });
