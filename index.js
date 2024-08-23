9d2cbb0488143bc632b3826f608928e349813c53
andrey <no_questions.dota2@mail.ru>
2024-08-23 21:50:39 +0300

    add additional-services

--- index.js
+++ index.js
@@ -23,11 +23,20 @@ const oversizedCargoesTransportation = document.querySelector(
   ".services__oversized-cargoes-transportation"
 );
 
-//
+/*  */
+/* ADDITIONAL-SERVICES */
+/*  */
+
+const insurance = document.querySelector(".cards__insurance");
+const foreignEconomicActivity = document.querySelector(
+  ".cards__foreignEconomicActivity"
+);
+const consulting = document.querySelector(".cards__consulting");
+const customsClearance = document.querySelector(".cards__customsClearance");
+
 /*  */
 /* INTERACTION */
 /*  */
-//
 
 /*  */
 /* HEADER */
@@ -81,7 +90,7 @@ seaFreight.addEventListener("click", () => {
   ul.append(li4);
 
   const modalLeftButton = document.createElement("button");
-  modalLeftButton.classList.add("modal__button");
+  modalLeftButton.classList.add("modal__button", "services-button");
   modalLeftButton.textContent = "Рассчитать стоимость";
   modalLeft.append(modalLeftButton);
 
@@ -124,7 +133,7 @@ railwayFreight.addEventListener("click", () => {
   ul.append(li4);
 
   const modalLeftButton = document.createElement("button");
-  modalLeftButton.classList.add("modal__button");
+  modalLeftButton.classList.add("modal__button", "services-button");
   modalLeftButton.textContent = "Рассчитать стоимость";
   modalLeft.append(modalLeftButton);
 
@@ -180,7 +189,7 @@ airFreight.addEventListener("click", () => {
   ul.append(li7);
 
   const modalLeftButton = document.createElement("button");
-  modalLeftButton.classList.add("modal__button");
+  modalLeftButton.classList.add("modal__button", "services-button");
   modalLeftButton.textContent = "Рассчитать стоимость";
   modalLeft.append(modalLeftButton);
 
@@ -223,7 +232,7 @@ roadFreight.addEventListener("click", () => {
   ul.append(li4);
 
   const modalLeftButton = document.createElement("button");
-  modalLeftButton.classList.add("modal__button");
+  modalLeftButton.classList.add("modal__button", "services-button");
   modalLeftButton.textContent = "Рассчитать стоимость";
   modalLeft.append(modalLeftButton);
 
@@ -273,7 +282,7 @@ oversizedCargoesTransportation.addEventListener("click", () => {
   ul.append(li4);
 
   const modalLeftButton = document.createElement("button");
-  modalLeftButton.classList.add("modal__button");
+  modalLeftButton.classList.add("modal__button", "services-button");
   modalLeftButton.textContent = "Рассчитать стоимость";
   modalLeft.append(modalLeftButton);
 
@@ -319,11 +328,210 @@ warehousingServices.addEventListener("click", () => {
   ul.append(li4);
 
   const modalLeftButton = document.createElement("button");
-  modalLeftButton.classList.add("modal__button");
+  modalLeftButton.classList.add("modal__button", "services-button");
   modalLeftButton.textContent = "Рассчитать стоимость";
   modalLeft.append(modalLeftButton);
 
   const modalRight = document.createElement("div");
   modalRight.classList.add("modal__right-warehousing", "modal__right");
+  document.querySelector(".modal").append(modalRight);
+});
+
+/*  */
+/* ADDITIONAL-SERVICES */
+/*  */
+
+insurance.addEventListener("click", () => {
+  createModal(content, "insurance");
+
+  const modalLeft = document.createElement("div");
+  modalLeft.classList.add("modal__left-insurance", "modal__left");
+  document.querySelector(".modal").append(modalLeft);
+
+  const modalLeftTitle = document.createElement("h1");
+  modalLeftTitle.classList.add("modal__insurance-title", "modal__title");
+  modalLeftTitle.textContent = "Страхование";
+  modalLeft.append(modalLeftTitle);
+
+  const ul = document.createElement("ul");
+  ul.classList.add("ul-insurance", "modal__ul");
+  modalLeft.append(ul);
+
+  const li1 = document.createElement("li");
+  li1.textContent =
+    "Покрытие рисков утраты (гибели) или повреждения груза во время транспортировки;";
+  ul.append(li1);
+
+  const li2 = document.createElement("li");
+  li2.textContent =
+    "Индивидуальные страховые программы, адаптированные под потребности клиентов и характер груза;";
+  ul.append(li2);
+
+  const li3 = document.createElement("li");
+  li3.textContent =
+    "Консультации в выборе оптимального типа страхования в соответствии с характером, стоимостью груза и условиями перевозки;";
+  ul.append(li3);
+
+  const li4 = document.createElement("li");
+  li4.textContent =
+    "Профессиональная поддержка при оформлении страховых полисов и урегулировании убытков;";
+  ul.append(li4);
+
+  const li5 = document.createElement("li");
+  li5.textContent =
+    "Вооруженное сопровождение особо ценных и дорогостоящих грузов.";
+  ul.append(li5);
+
+  const modalLeftButton = document.createElement("button");
+  modalLeftButton.classList.add("modal__button", "additional-services-button");
+  modalLeftButton.textContent = "Рассчитать стоимость";
+  modalLeft.append(modalLeftButton);
+
+  const modalRight = document.createElement("div");
+  modalRight.classList.add("modal__right-insurance", "modal__right");
+  document.querySelector(".modal").append(modalRight);
+});
+
+foreignEconomicActivity.addEventListener("click", () => {
+  createModal(content, "foreign");
+
+  const modalLeft = document.createElement("div");
+  modalLeft.classList.add("modal__left-foreign", "modal__left");
+  document.querySelector(".modal").append(modalLeft);
+
+  const modalLeftTitle = document.createElement("h1");
+  modalLeftTitle.classList.add("modal__foreign-title", "modal__title");
+  modalLeftTitle.textContent = "ВЭД";
+  modalLeft.append(modalLeftTitle);
+
+  const ul = document.createElement("ul");
+  ul.classList.add("ul-foreign", "modal__ul");
+  modalLeft.append(ul);
+
+  const li1 = document.createElement("li");
+  li1.textContent =
+    "Анализ, структурирование и создание архитектуры внешнеэкономических сделок;";
+  ul.append(li1);
+
+  const li2 = document.createElement("li");
+  li2.textContent =
+    "Подготовка двуязычных договоров на русском и английском языках;";
+  ul.append(li2);
+
+  const li3 = document.createElement("li");
+  li3.textContent =
+    "Подбор банков в РФ и за ее пределами и согласование условий осуществления оплат по внешнеэкономическим сделкам;";
+  ul.append(li3);
+
+  const li4 = document.createElement("li");
+  li4.textContent =
+    "Подготовка документов для прохождение банковского комплаенса.";
+  ul.append(li4);
+
+  const modalLeftButton = document.createElement("button");
+  modalLeftButton.classList.add("modal__button", "additional-services-button");
+  modalLeftButton.textContent = "Рассчитать стоимость";
+  modalLeft.append(modalLeftButton);
+
+  const modalRight = document.createElement("div");
+  modalRight.classList.add("modal__right-foreign", "modal__right");
+  document.querySelector(".modal").append(modalRight);
+});
+
+consulting.addEventListener("click", () => {
+  createModal(content, "consulting");
+
+  const modalLeft = document.createElement("div");
+  modalLeft.classList.add("modal__left-consulting", "modal__left");
+  document.querySelector(".modal").append(modalLeft);
+
+  const modalLeftTitle = document.createElement("h1");
+  modalLeftTitle.classList.add("modal__consulting-title", "modal__title");
+  modalLeftTitle.textContent = "Консалтинг";
+  modalLeft.append(modalLeftTitle);
+
+  const ul = document.createElement("ul");
+  ul.classList.add("ul-consulting", "modal__ul");
+  modalLeft.append(ul);
+
+  const li1 = document.createElement("li");
+  li1.textContent = "Анализ и оптимизация логистических процессов;";
+  ul.append(li1);
+
+  const li2 = document.createElement("li");
+  li2.textContent =
+    "Оценка эффективности цепей поставок и предложение решений по их улучшению;";
+  ul.append(li2);
+
+  const li3 = document.createElement("li");
+  li3.textContent = "Консультации по управлению всеми видами перевозок;";
+  ul.append(li3);
+
+  const li4 = document.createElement("li");
+  li4.textContent = "ИТ интеграция с клиентами;";
+  ul.append(li4);
+
+  const li5 = document.createElement("li");
+  li5.textContent = "Решения для управления складами.";
+  ul.append(li5);
+
+  const modalLeftButton = document.createElement("button");
+  modalLeftButton.classList.add("modal__button", "additional-services-button");
+  modalLeftButton.textContent = "Рассчитать стоимость";
+  modalLeft.append(modalLeftButton);
+
+  const modalRight = document.createElement("div");
+  modalRight.classList.add("modal__right-consulting", "modal__right");
+  document.querySelector(".modal").append(modalRight);
+});
+
+customsClearance.addEventListener("click", () => {
+  createModal(content, "customs");
+
+  const modalLeft = document.createElement("div");
+  modalLeft.classList.add("modal__left-customs", "modal__left");
+  document.querySelector(".modal").append(modalLeft);
+
+  const modalLeftTitle = document.createElement("h1");
+  modalLeftTitle.classList.add("modal__customs-title", "modal__title");
+  modalLeftTitle.textContent = "Таможенное оформление";
+  modalLeft.append(modalLeftTitle);
+
+  const ul = document.createElement("ul");
+  ul.classList.add("ul-customs", "modal__ul");
+  modalLeft.append(ul);
+
+  const li1 = document.createElement("li");
+  li1.textContent =
+    "Осуществление таможенного декларирования грузов и организация терминальной обработки;";
+  ul.append(li1);
+
+  const li2 = document.createElement("li");
+  li2.textContent =
+    "Профессиональное сопровождение всех этапов таможенного оформления грузов;";
+  ul.append(li2);
+
+  const li3 = document.createElement("li");
+  li3.textContent =
+    "Компетенции по применению таможенных процедур, отличных от импорта (ИМ40) / экспорта (ЭК10);";
+  ul.append(li3);
+
+  const li4 = document.createElement("li");
+  li4.textContent =
+    "Консультирование по вопросам таможенного законодательства;";
+  ul.append(li4);
+
+  const li5 = document.createElement("li");
+  li5.textContent =
+    "Разработка оптимальных стратегий и схем таможенного оформления для минимизации рисков и оптимизации стоимости импорта/экспорта.";
+  ul.append(li5);
+
+  const modalLeftButton = document.createElement("button");
+  modalLeftButton.classList.add("modal__button", "additional-services-button");
+  modalLeftButton.textContent = "Рассчитать стоимость";
+  modalLeft.append(modalLeftButton);
+
+  const modalRight = document.createElement("div");
+  modalRight.classList.add("modal__right-customs", "modal__right");
   document.querySelector(".modal").append(modalRight);
 });
